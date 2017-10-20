1ca1dd931690502d741cb5d595497d3d1e8e381a
andrewdownie <andrew_dev@outlook.com>
2017-10-20 05:26:30 -0400

    Ended up doing it the other way around. ActiveEffect inherits from static effect, since active effect needs two additional fields, and static effect can just hardcode duration to 1. There is no vestigial code in the effect classes, and the only code duplication that exists, is roughly 6 lines in the DamageTick() method where the timer code needed to be added for ActiveEffects. This is already very clean, and I'm not sure how I would make it any cleaner, given I have to work with Unity's serialization limitations.

--- Assets/ActiveEffect.cs
+++ Assets/ActiveEffect.cs
@@ -1,64 +1,30 @@
 ﻿using UnityEngine;
 
 [System.Serializable]
-public class ActiveEffect : InstantEffect{
+public class ActiveEffect : StaticEffect{
 	[SerializeField]
-	float durationInSeconds;
+	protected float durationInSeconds;
 	[SerializeField]
-	TickDamageRateEnum damageRate;
-
-
-	[SerializeField]
-	float timePassed;
-	[SerializeField]
-	int ticksUntilNextDamage;
+	protected float timePassed;
 
 
 
-
-	float DamageRatio(){
-		/*
-		Calculates the ratio of how much damage should be done
-		per tick for damage over time (DOTs)
-		*/
-		float dmgRatio = 1 / ( ((float)damageRate) * durationInSeconds);
-		Debug.Log("Ratio: " + dmgRatio);
-		return dmgRatio;
-	}
-	
-	int TicksUntilNextDamage(){
-		int ticksUntilNextDamage = (int)(SEA.TICKS_PER_SECOND / ((float)damageRate));
-		Debug.Log("Ticks till damage: " + ticksUntilNextDamage);
-
-		return ticksUntilNextDamage;
-	}
-
-
-
-
-    public CombatStats? DamageTick(){
+	public override CombatStats? DamageTick(){
 		timePassed += SEA.MS_PER_TICK;
 		ticksUntilNextDamage = ticksUntilNextDamage - 1;
 
-		//Debug.Log(timePassed);
-
-
 		if(timePassed >= durationInSeconds){
-			Debug.Log("marker 1");
 			return null;
 		}
 
 		if(ticksUntilNextDamage < 1){
-			Debug.Log("marker 2");
 			ticksUntilNextDamage = TicksUntilNextDamage();
-			CombatStats? resultDamage = base.damage * DamageRatio();
-			Debug.Log("Damage per tick: " + resultDamage.Value[CombatStatEnum.normal]);
+			CombatStats? resultDamage = base.damage * DamageRatio(durationInSeconds);
 			return resultDamage;
 		}
 
 		return new CombatStats();
 	}
-
 
 
 }
--- Assets/StaticEffect.cs
+++ Assets/StaticEffect.cs
@@ -3,30 +3,23 @@
 [System.Serializable]
 public class StaticEffect : InstantEffect{
 	[SerializeField]
-	float durationInSeconds;
-	[SerializeField]
-	TickDamageRateEnum damageRate;
-
+	protected TickDamageRateEnum damageRate;
 
 	[SerializeField]
-	float timePassed;
-	[SerializeField]
-	int ticksUntilNextDamage;
+	protected int ticksUntilNextDamage;
 
 
-
-
-	float DamageRatio(){
+	protected float DamageRatio(float effectDuration){
 		/*
 		Calculates the ratio of how much damage should be done
 		per tick for damage over time (DOTs)
 		*/
-		float dmgRatio = 1 / ( ((float)damageRate) * durationInSeconds);
+		float dmgRatio = 1 / ( ((float)damageRate) * effectDuration);
 		Debug.Log("Ratio: " + dmgRatio);
 		return dmgRatio;
 	}
 	
-	int TicksUntilNextDamage(){
+	protected int TicksUntilNextDamage(){
 		int ticksUntilNextDamage = (int)(SEA.TICKS_PER_SECOND / ((float)damageRate));
 		Debug.Log("Ticks till damage: " + ticksUntilNextDamage);
 
@@ -34,31 +27,16 @@ public class StaticEffect : InstantEffect{
 	}
 
 
-
-
-    public CombatStats? DamageTick(){
-		timePassed += SEA.MS_PER_TICK;
+	public virtual CombatStats? DamageTick(){
 		ticksUntilNextDamage = ticksUntilNextDamage - 1;
 
-		//Debug.Log(timePassed);
-
-
-		if(timePassed >= durationInSeconds){
-			Debug.Log("marker 1");
-			return null;
-		}
-
 		if(ticksUntilNextDamage < 1){
-			Debug.Log("marker 2");
 			ticksUntilNextDamage = TicksUntilNextDamage();
-			CombatStats? resultDamage = base.damage * DamageRatio();
-			Debug.Log("Damage per tick: " + resultDamage.Value[CombatStatEnum.normal]);
+			CombatStats? resultDamage = base.damage * DamageRatio(1);
 			return resultDamage;
 		}
 
 		return new CombatStats();
 	}
-
-
 
 }
